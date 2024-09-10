91428adc95f4b255cc41adb0ff71ef7ae9606d5d
Klas Jersevi <klas@weavy.com>
2024-09-10 12:21:45 +0200

    packages update

--- src/components/weavy/notifications.tsx
+++ src/components/weavy/notifications.tsx
@@ -1,18 +1,10 @@
 "use client"
 import React, { useState } from "react"
 import { Button, Drawer } from "antd"
-import { WyNotifications } from "@weavy/uikit-react"
+import { ConversationTypes, WyNotifications } from "@weavy/uikit-react"
 import { WeavyThemeProvider } from "@contexts/weavy/theme"
 import { BellOutlined } from "@ant-design/icons"
 import { useGo, useParsed } from "@refinedev/core"
-
-export enum ConversationTypeGuid {
-  ChatRoom = "edb400ac-839b-45a7-b2a8-6a01820d1c44",
-  //ContextualChat = "d65dd4bc-418e-403c-9f56-f9cf4da931ed",
-  PrivateChat = "7e14f418-8f15-46f4-b182-f619b671e470",
-  BotChat = "2352a1c6-abc6-420e-8b85-ca7d5aed8779",
-}
-export const ConversationTypes = new Map(Object.entries(ConversationTypeGuid))
 
 export const WeavyNotifications: React.FC = () => {
   const [open, setOpen] = useState(false)
